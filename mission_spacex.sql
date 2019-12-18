a5a97d28985665405bb9b848992c07d97679fe5d
FALGUN PATEL <falgunrpatel@hotmail.com>
2019-12-17 19:35:09 -0500

    cores table added

--- mission_spacex.sql
+++ mission_spacex.sql
@@ -17,3 +17,22 @@ create table if not exists capsule_mission(
 	name char(10),
 	flight int
 );
+
+create table if not exists cores(
+ 	core_serial varchar(10) primary key,
+	block int,
+	status varchar(10),
+	original_launch TIMESTAMP,
+	reuse_count int,
+	rtls_attempts int,
+	rtls_landings int,
+	asds_attempts int,
+	asds_landings int,
+	water_landing int
+);
+
+create table if not exists cores_mission(
+	core_serial  varchar(10) references cores(core_serial),
+	name char(10),
+	flight int	
+);
