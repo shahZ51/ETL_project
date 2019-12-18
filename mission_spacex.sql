ff47eed421ae5a75c31cadbb08e7e9e7d4cb76b6
FALGUN PATEL <falgunrpatel@hotmail.com>
2019-12-17 21:32:19 -0500

    updated schema latest by 9:30

--- mission_spacex.sql
+++ mission_spacex.sql
@@ -5,7 +5,7 @@ create table if not exists capsules(
 	capsule_serial char(4) primary key,
 	capsule_id varchar(10),
 	status varchar(10),
-	original_launch TIMESTAMP,
+	original_launch date,
 	landings bit,
 	type varchar(15),
 	details varchar(50),
@@ -13,7 +13,7 @@ create table if not exists capsules(
 );
 drop table if exists capsule_mission;
 create table if not exists capsule_mission(
-	capsule_mission_id serial primary_key,
+	capsule_mission_id serial primary key,
 	capsule_serial char(4) references capsules(capsule_serial),
 	name char(10),
 	flight int
@@ -23,7 +23,7 @@ create table if not exists cores(
  	core_serial varchar(10) primary key,
 	block int,
 	status varchar(10),
-	original_launch TIMESTAMP,
+	original_launch date,
 	reuse_count int,
 	rtls_attempts int,
 	rtls_landings int,
@@ -33,8 +33,58 @@ create table if not exists cores(
 );
 drop table if exists cores_mission;
 create table if not exists cores_mission(
-	cores_mission_id serial primary key
+	cores_mission_id serial primary key,
 	core_serial  varchar(10) references cores(core_serial),
 	name char(10),
 	flight int	
+);
+drop table if exists dragons;
+create table if not exists dragons(
+	id varchar(10) primary key,
+	name varchar(15),
+	type varchar(15),
+	active bit,
+	crew_capacity int,
+	sidewall_angle_deg int,
+	orbit_duration_yr int,
+	dry_mass_kg int,
+	dry_mass_lb int,
+	first_flight date,
+	wikipedia text,
+	description text
+);
+drop table if exists dragons;
+create table if not exists dragons(
+	id varchar(10) primary key,
+	name varchar(15),
+	type varchar(15),
+	active bit,
+	crew_capacity int,
+	sidewall_angle_deg int,
+	orbit_duration_yr int,
+	dry_mass_kg int,
+	dry_mass_lb int,
+	first_flight date,
+	wikipedia text,
+	description text
+);
+drop table if exists thrusters;
+create table if not exists thrusters(
+	id serial primary key,
+	dragon_id varchar(10) references dragons(id),
+	type varchar(10),
+	amount float,
+	pods int,
+	fuel_1 varchar(50),
+	fuel_2 varchar(50),
+	thurst_kn float,
+	thurst_lbf float
+);
+drop table if exists history;
+create table if not exists history(
+	id int primary key,
+	title varchar(50),
+	event_date date,
+	flight_number int,
+	details text
 );
