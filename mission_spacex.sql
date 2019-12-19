2df0cfee05f738bad7819dbdb40d52a462c91947
FALGUN PATEL <falgunrpatel@hotmail.com>
2019-12-18 21:32:46 -0500

    semi-final mission spacex schema

--- mission_spacex.sql
+++ mission_spacex.sql
@@ -102,6 +102,7 @@ create table if not exists landpads(
 	id varchar(5) primary key,
 	full_name varchar(25),
 	status varchar(10),
+	location json,
 	landing_type varchar(5),
 	attempted_landings int,
 	successful_landings int,
@@ -110,14 +111,7 @@ create table if not exists landpads(
 );
 
 drop table if exists landpads_location;
-create table if not exists landpads_location(
-	id serial primary key,
-	landpads_id varchar(5) references landpads(id),
-	name varchar(25),
-	region varchar(25),
-	latitude  float,
-	longitude float
-);
+
 drop table if exists ships;
 create table if not exists ships(
 	ship_id varchar(25) primary key,
@@ -144,4 +138,96 @@ create table if not exists ships(
 	missions text [],
 	url text,
 	image text
+)
+
+drop table if exists rockets;
+create table if not exists rockets(
+	id int,
+	active boolean,
+	stages int,
+	bosters int,
+	cost_per_launch int,
+	success_rate_pct int,
+	first_flight date,
+	country text,
+	company varchar(25),
+	height json,
+	diameter json,
+	mass json,
+	payload_weights json,
+	first_stage json,
+	second_stage json,
+	engines json,
+	landing_legs json,
+	flickr_images json,
+	wikipedia text,
+	description text,
+	rocket_id varchar(10) primary key,
+	rocket_name varchar(10),
+	rocket_type varchar(10)
+);
+
+
+drop table if exists payloads;
+create table if not exists payloads(
+	payload_id varchar(15) primary key,
+	norad_id int [] NULL,
+	reused boolean,
+	customers text [],
+	nationality varchar(25),
+	manufacturer varchar(5),
+	payload_type varchar(15),
+	payload_mass_kgs int,
+	payload_mass_lbs int,
+	orbit char(5),
+	orbit_params json	
+);
+
+drop table if exists missions;
+create table if not exists missions(
+	mission_name varchar(15),
+	mission_id varchar(15) primary key,
+	manufacturer text [],
+	payload_ids text [],
+	wikipedia text,
+	website text,
+	twitter text,
+	description text
+);
+
+drop table if exists launchpads;
+create table if not exists launchpads(
+	id int primary key,
+	name varchar(25),
+	status varchar(10),
+	location json,
+	vehicles_launched text [],
+	attempted_launches int,
+	successful_launches int,
+	wikipedia text,
+	details text,
+	site_id varchar(25),
+	site_name_long text
+);
+
+drop table if exists launches;
+create table if not exists launches(
+	flight_number int primary key,
+	mission_name varchar(25),
+	mission_id int [] null,
+	upcoming boolean,
+	launch_year varchar(4),
+	launch_date date,
+	is_tenative boolean,
+	tentative_max_precision varchar(5),
+	tbd boolean,
+	launch_window int,
+	rocket_id varchar(10) references rockets(rocket_id),
+	ships varchar(25) references ships(ship_id),	
+	launchsite json,
+	launch_success boolean,
+	launch_failure_details json,
+	links json,
+	details text,
+	static_fire_date_utc varchar(25)
 )
